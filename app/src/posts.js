959ecb6291aabafd39967f184ec080557b0b7e58
abrahamrhoffman <abrahamrhoffman@gmail.com>
2018-11-19 16:36:19 -0700

    0.1

--- app/src/posts.js
+++ app/src/posts.js
@@ -5,12 +5,25 @@ import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-adm
 export const PostList = props => (
     <List {...props}>
         <Datagrid>
-            <TextField source="id" />
-            <ReferenceField source="userId" reference="users">
-              <TextField source="name" />
-            </ReferenceField>
-            <TextField source="title" />
-            <EditButton />
+          <TextField source="id" />
+          <ReferenceField source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="title" />
+          <EditButton />
         </Datagrid>
     </List>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+          <DisabledInput source="id" />
+          <ReferenceInput source="userId" reference="users">
+            <SelectInput optionText="name" />
+          </ReferenceInput>
+          <TextInput source="title" />
+          <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
 );
