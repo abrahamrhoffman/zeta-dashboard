bd3caa844368321ae1a270223595f445b4d64d8f
abrahamrhoffman <abrahamrhoffman@gmail.com>
2018-11-19 16:27:21 -0700

    0.1

--- app/src/posts.js
+++ app/src/posts.js
@@ -1,16 +1,17 @@
 // in src/posts.js
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
 
 export const PostList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
+        <Datagrid>
+            <TextField source="id" />
             <ReferenceField source="userId" reference="users">
-                <TextField source="id" />
+              <TextField source="name" />
             </ReferenceField>
-            <TextField source="name" />
             <TextField source="title" />
-            <TextField source="body" />
+            <EditButton />
         </Datagrid>
     </List>
 );
