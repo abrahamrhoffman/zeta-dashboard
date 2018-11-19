ff9edd0c820831e4908713937777ffbefa4db8ee
abrahamrhoffman <abrahamrhoffman@gmail.com>
2018-11-19 16:58:33 -0700

    0.1

--- app/src/App.js
+++ app/src/App.js
@@ -4,13 +4,13 @@ import { PostList } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 
-import PostIcon from '@material-ui/icons/DeviceHub';
+import PostIcon from '@material-ui/icons/ChevronRight';
 import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} icon={PostIcon} />
+        <Resource name="functions" list={PostList} icon={PostIcon} />
         <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
