c7d25d973613450c748584bdacbdae92326b16bc
abrahamrhoffman <abrahamrhoffman@gmail.com>
2018-11-26 07:02:28 -0700

    0.2

--- app/src/App.js
+++ app/src/App.js
@@ -3,25 +3,21 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import FunctionsIcon from '@material-ui/icons/Code';
 {/*
+
+  Notes ::
+
   import { Admin, Resource, EditGuesser } from 'react-admin';
   import { PostList } from './posts';
   import { UserList } from './users';
-*/}
-{/*
   import PostIcon from '@material-ui/icons/Code';
   import UserIcon from '@material-ui/icons/Group';
-*/}
 
-{/*
-  Zeta API Custom Data Provider
-*/}
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-
-{/*
-  Testing data provider
+  Zeta API Custom Data Provider :: Testing
   Example: const dataProvider = (type, resource, params) => new Promise();
+
 */}
 
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
         <Resource name="functions" icon={FunctionsIcon} />
